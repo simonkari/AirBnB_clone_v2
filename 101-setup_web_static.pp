8dc82c0289d130e9a1645912b86ffb13c90ce149
simonkari <kariithisimon1715@gmail.com>
2023-09-07 21:25:22 +0300

    101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,59 +1,91 @@
-class nginx_config {
-  # Install Nginx
-  package { 'nginx':
-    ensure => 'installed',
-  }
+# Configures a web server for deployment of web_static.
 
-  # Create directories
-  file { ['/data/web_static/releases/test/', '/data/web_static/shared/']:
-    ensure => 'directory',
-    owner  => 'ubuntu',
-    group  => 'ubuntu',
-    mode   => '0755',
-  }
-
-  # Create index.html file
-  file { '/data/web_static/releases/test/index.html':
-    ensure  => 'file',
-    content => 'Holberton School',
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-    mode    => '0644',
-    require => File['/data/web_static/releases/test/'],
-  }
-
-  # Create a symbolic link
-  file { '/data/web_static/current':
-    ensure => 'link',
-    target => '/data/web_static/releases/test/',
-  }
-
-  # Define an Nginx location block
-  file { '/etc/nginx/sites-available/default':
-    ensure  => 'file',
-    content => template('nginx_config/default.erb'),
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
-
-  # Start and enable Nginx service
-  service { 'nginx':
-    ensure    => 'running',
-    enable    => true,
-    require   => Package['nginx'],
-    subscribe => File['/etc/nginx/sites-available/default'],
-  }
-}
-
-server {
+# Nginx configuration file
+$nginx_conf = "server {
     listen 80 default_server;
     listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
 
-    root /data/web_static/current;
-
-    location /hbnb_static/ {
-        alias /data/web_static/current/;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
     }
 
-    # ... other server configurations ...
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+} ->
+
+file { '/data':
+  ensure  => 'directory'
+} ->
+
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "Holberton School Puppet\n"
+} ->
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+
+file { '/var/www':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
