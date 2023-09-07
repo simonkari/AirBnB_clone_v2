31a7823533221308e7c824e46f99e421825c5f41
simonkari <kariithisimon1715@gmail.com>
2023-09-07 20:20:51 +0300

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,21 +3,17 @@
 from datetime import datetime
 from fabric.api import local
 
+
 def do_pack():
     """
-    Create a compressed archive of the 'web_static' folder.
-
-    Returns:
-        str: The path to the compressed archive if successful, None otherwise.
+    must return the archive path if the archive has been correctly
+    generated. Otherwise, it should return None
     """
     try:
-        with lcd("versions"):
-            local("mkdir -p versions")
-            date = datetime.now().strftime("%Y%m%d%H%M%S")
-            archive_name = "web_static_{}.tgz".format(date)
-            archive_path = "versions/{}".format(archive_name)
-            local("tar -cvzf {} web_static".format(archive_path))
-        return archive_path
-    except Exception as e:
-        print("Error:", e)
+        local("mkdir -p versions")
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        rout = "versions/web_static_{}.tgz".format(date)
+        _gzip = local("tar -cvzf {} web_static".format(rout))
+        return rout
+    except Exception:
         return None
