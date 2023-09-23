ebd416d3ee35c8f21a86755e3e8a56777e1734fe
simonkari <kariithisimon1715@gmail.com>
2023-09-23 09:29:05 +0300

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python3
-'''A simple Flask web application.'''
+
+'''
+Create a Flask application instance
+'''
 from flask import Flask
+
 
 app = Flask(__name__)
 
+'''
+The instance of the Flask application.
+'''
+
+app.url_map.strict_slashes = False
+
+
 @app.route('/')
 def index():
-    '''The home page.'''
+    '''
+    home page.
+    '''
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hbnb():
-    '''The hbnb page.'''
+    '''
+    hbnb page.
+    '''
     return 'HBNB'
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
