b125549c94c56e06b3813ea6d57c672dce3d16e7
simonkari <kariithisimon1715@gmail.com>
2023-09-23 09:31:40 +0300

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,24 +3,29 @@
 '''
 from flask import Flask
 
-app = Flask(__name__)  # The Flask application instance
+
+app = Flask(__name__)
+'''The Flask application instance.'''
 app.url_map.strict_slashes = False
+
 
 @app.route('/')
 def index():
     '''The home page.'''
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
 def hbnb():
     '''The hbnb page.'''
     return 'HBNB'
 
+
 @app.route('/c/<text>')
 def c_page(text):
     '''The c page.'''
-    formatted_text = text.replace('_', ' ')
-    return f'C {formatted_text}'
+    return 'C {}'.format(text.replace('_', ' '))
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
