844b6d37595bd35de6e2d4832ad7759875f263b4
simonkari <kariithisimon1715@gmail.com>
2023-09-23 09:50:32 +0300

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,35 +1,51 @@
 #!/usr/bin/python3
-'''A simple Flask web application.
+
 '''
+A straightforward Flask web application.
+'''
+
 from flask import Flask
 
-app = Flask(__name__)
 
-# Disable strict slashes
+app = Flask(__name__)
+'''
+The instance of the Flask application.
+'''
 app.url_map.strict_slashes = False
 
-# Define routes and views
+
 @app.route('/')
 def index():
-    '''The home page.'''
+    '''
+    The main page.
+    '''
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hbnb():
-    '''The hbnb page.'''
+    '''
+    hbnb page.
+    '''
     return 'HBNB'
+
 
 @app.route('/c/<text>')
 def c_page(text):
-    '''The c page.'''
+    '''
+    c page.
+    '''
     return 'C {}'.format(text.replace('_', ' '))
 
+
+@app.route('/python/', defaults={'text': 'is cool'})
 @app.route('/python/<text>')
-@app.route('/python', defaults={'text': 'is cool'})
 def python_page(text):
     '''The python page.'''
-    return 'Python {}'.format(text.replace('_', ' '))
+    formatted_text = text.replace('_', ' ')
+    return f'Python {formatted_text}'
 
-# Run the application if executed as a script
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
