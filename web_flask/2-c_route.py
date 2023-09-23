a1782688766bb3c4cf8ad23a22bc6dae47b85995
simonkari <kariithisimon1715@gmail.com>
2023-09-23 09:30:56 +0300

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''A simple Flask web application.
+'''
+from flask import Flask
+
+app = Flask(__name__)  # The Flask application instance
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def index():
+    '''The home page.'''
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hbnb():
+    '''The hbnb page.'''
+    return 'HBNB'
+
+@app.route('/c/<text>')
+def c_page(text):
+    '''The c page.'''
+    formatted_text = text.replace('_', ' ')
+    return f'C {formatted_text}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
