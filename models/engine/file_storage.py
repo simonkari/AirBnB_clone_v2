2b49ad7e02a6e36152c3b37604b515d889b45cce
simonkari <kariithisimon1715@gmail.com>
2023-09-24 12:10:42 +0300

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""
+This module defines a class to manage file storage
+for hbnb clone
+"""
 import json
 import os
 from importlib import import_module
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """
+    This class manages storage of hbnb models in
+    JSON format
+    """
     __file_path = 'file.json'
     __objects = {}
 
     def __init__(self):
-        """Initializes a FileStorage instance"""
+        """
+        Initializes a FileStorage instance
+        """
         self.model_classes = {
             'BaseModel': import_module('models.base_model').BaseModel,
             'User': import_module('models.user').User,
@@ -23,7 +31,9 @@ class FileStorage:
         }
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
+        """
+        Returns a dictionary of models currently in storage
+        """
         if cls is None:
             return self.__objects
         else:
@@ -34,20 +44,26 @@ class FileStorage:
             return filtered_dict
 
     def delete(self, obj=None):
-        """Removes an object from the storage dictionary"""
+        """
+        Removes an object from the storage dictionary
+        """
         if obj is not None:
             obj_key = obj.to_dict()['__class__'] + '.' + obj.id
             if obj_key in self.__objects.keys():
                 del self.__objects[obj_key]
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
+        """
+        Adds new object to storage dictionary
+        """
         self.__objects.update(
             {obj.to_dict()['__class__'] + '.' + obj.id: obj}
         )
 
     def save(self):
-        """Saves storage dictionary to file"""
+        """
+        Saves storage dictionary to file
+        """
         with open(self.__file_path, 'w') as file:
             temp = {}
             for key, val in self.__objects.items():
@@ -55,7 +71,9 @@ class FileStorage:
             json.dump(temp, file)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """
+        Loads storage dictionary from file
+        """
         classes = self.model_classes
         if os.path.isfile(self.__file_path):
             temp = {}
@@ -65,5 +83,7 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
 
     def close(self):
-        """Closes the storage engine."""
+        """
+        Closes the storage engine.
+        """
         self.reload()
