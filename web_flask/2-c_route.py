f92d8ef2b04efac6bcafe263c0483e107b1d55b7
simonkari <kariithisimon1715@gmail.com>
2023-09-24 16:54:13 +0300

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,30 +1,42 @@
 #!/usr/bin/python3
-""" Hello world in flask"""
 
+'''
+A basic web application built with Flask.
+'''
 
 from flask import Flask
 
 
 app = Flask(__name__)
+'''
+The instance of the Flask application
+'''
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello_world():
-    """route index"""
+@app.route('/')
+def index():
+    '''
+    Home page.
+    '''
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    """route HBNB"""
+    '''
+    hbnb page.
+    '''
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def ctext(text):
-    """route C"""
-    return 'C %s' % text.replace('_', ' ')
+@app.route('/c/<text>')
+def c_page(text):
+    '''
+    c page.
+    '''
+    return 'C {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port='5000')
