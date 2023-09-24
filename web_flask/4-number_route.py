46184c85e3b82dc18a9116742d2ba82fda66efab
simonkari <kariithisimon1715@gmail.com>
2023-09-24 17:06:14 +0300

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,43 +1,59 @@
 #!/usr/bin/python3
-""" Hello world in flask"""
+""" 
+Hello World in Flask
+"""
 
-
+# Import the Flask framework
 from flask import Flask
 
-
+# Create a Flask application instance
 app = Flask(__name__)
 
-
+# Define a route for the root URL '/'
 @app.route('/', strict_slashes=False)
 def hello_world():
-    """route index"""
+    """
+    Route for the root URL
+    """
     return 'Hello HBNB!'
 
-
+# Define a route for the URL '/hbnb'
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """route HBNB"""
+    """
+    Route for '/hbnb'
+    """
     return 'HBNB'
 
-
+# Define a route for URLs starting with '/c/' followed by a variable 'text'
 @app.route('/c/<text>', strict_slashes=False)
 def ctext(text):
-    """route C"""
+    """
+    Route for '/c/<text>'
+    Replaces underscores in 'text' with spaces
+    """
     return 'C %s' % text.replace('_', ' ')
 
-
+# Define a route for the URL '/python' with an optional variable 'text'
+# Defaults to 'is cool' if 'text' is not provided
 @app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def ctextdefault(text):
-    """route C"""
+    """
+    Route for '/python' with an optional 'text' parameter
+    Replaces underscores in 'text' with spaces
+    """
     return 'Python %s' % text.replace('_', ' ')
 
-
+# Define a route for URLs starting with '/number/' followed by an integer 'n'
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """route number"""
+    """
+    Route for '/number/<int:n>'
+    Responds with a message indicating that 'n' is a number
+    """
     return '%d is a number' % n
 
-
+# Run the Flask application if this script is the main program
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
