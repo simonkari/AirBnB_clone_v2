847bc6bc1044e44d94b066cf0bc036d0abf6ebf2
simonkari <kariithisimon1715@gmail.com>
2023-09-24 13:22:19 +0300

    db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""
-"""
+'''change your storage engine and use SQLAlchemy'''
+
+
+from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from os import getenv
-from os import environ
-from models.base_model import BaseModel, Base
+from models.base_model import Base
 from models.user import User
 from models.state import State
 from models.city import City
@@ -13,59 +13,66 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
+import models
+"""This module defines a class to manage sqlachemy for hbnb clone"""
+
 
 class DBStorage():
-    """
-    """
+    '''Database storage'''
     __engine = None
     __session = None
-    cla = [State, City, User, Place, Review, Amenity]
 
     def __init__(self):
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
-                                      format(getenv("HBNB_MYSQL_USER"),
-                                             getenv("HBNB_MYSQL_PWD"),
-                                             getenv("HBNB_MYSQL_HOST"),
-                                             getenv("HBNB_MYSQL_DB")),
-                                      pool_pre_ping=True)
-        if 'HBNB_ENV' in environ and environ['HBNB_ENV'] == 'test':
-            Base.metadata.drop_all(bind=self.__engine)
+        '''Getting attributes for the create engine Url'''
+        user = getenv('HBNB_MYSQL_USER')
+        pas = getenv('HBNB_MYSQL_PWD')
+        host = getenv('HBNB_MYSQL_HOST', 'localhost')
+        db = getenv('HBNB_MYSQL_DB')
+        env = getenv('HBNB_ENV')
+        self.__engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}'.format(
+                user, pas, host, db), pool_pre_ping=True)
+        if env == 'test':
+            Base.metadata.drop_all(self.__engine)
+            # engine specify database connection where operations are perfomed
 
     def all(self, cls=None):
-        """all"""
-        dic = {}
-        if cls is None:
-            for c in self.cla:
-                row = self.__session.query(c).all()
-                for k in row:
-                    key = ".".join([c.__name__, k.id])
-                    dic.update({key: k})
-            return dic
-        row = self.__session.query(eval(cls)).all()
-        for k in row:
-            key = ".".join([cls, k.id])
-            dic.update({key: k})
-        return dic
+        '''Retrieve all objects or individual class objects'''
+
+        my_classes = [User, State, City, Amenity, Place, Review]
+        dictionary = {}
+        if cls is not None:
+            for value in self.__session.query(cls).all():
+                key = '{}.{}'.format(value.__class__.__name__, value.id)
+                dictionary[key] = value
+        else:
+            for clas in my_classes:
+                for value in self.__session.query(clas).all():
+                    key = '{}.{}'.format(value.__class__.__name__, value.id)
+                    dictionary[key] = value
+        return dictionary
 
     def new(self, obj):
-        """new"""
+        '''Add a new object to the database'''
         self.__session.add(obj)
 
     def save(self):
-        """save"""
+        '''Implement data persistence'''
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete"""
+        '''Delete an entire database'''
         if obj is not None:
-            self.__session.delete(obj)
+            Base.metadata.drop_all(self.__engine)
+            # Deleting a database is basically dropping all the tables
 
     def reload(self):
-        """reload"""
+        '''Retrieves all the data database'''
         Base.metadata.create_all(self.__engine)
-        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        self.__session = scoped_session(Session)()
+        Var_Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(Var_Session)
+        self.__session = Session()
 
     def close(self):
-        """close"""
+        '''Calls remove() on private session attr'''
         self.__session.close()
