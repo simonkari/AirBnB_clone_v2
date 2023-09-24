bd13d1710f2f0f0ed0987cb1618df94b369e8c49
simonkari <kariithisimon1715@gmail.com>
2023-09-24 17:22:13 +0300

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,50 +1,57 @@
 #!/usr/bin/python3
-""" 
-Hello World in Flask
-"""
 
-# Import the Flask framework
+'''
+A straightforward Flask web application.
+'''
+
 from flask import Flask
 
-# Create a Flask application instance
-app = Flask(__name__)
 
-# Define a route for the root URL '/'
-@app.route('/', strict_slashes=False)
-def hello_world():
-    """
-    Route for the root URL
-    """
+app = Flask(__name__)
+'''
+The instance of the Flask application.
+'''
+
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    '''
+    The main page.
+    '''
+
     return 'Hello HBNB!'
 
-# Define a route for the URL '/hbnb'
-@app.route('/hbnb', strict_slashes=False)
+
+@app.route('/hbnb')
 def hbnb():
-    """
-    Route for '/hbnb'
-    """
+    '''
+    hbnb page.
+    '''
+
     return 'HBNB'
 
-# Define a route for URLs starting with '/c/' followed by a variable 'text'
-@app.route('/c/<text>', strict_slashes=False)
-def ctext(text):
-    """
-    Route for '/c/<text>'
-    Replaces underscores in 'text' with spaces
-    """
-    return 'C %s' % text.replace('_', ' ')
 
-# Define a route for the URL '/python' with an optional variable 'text'
-# Defaults to 'is cool' if 'text' is not provided
-@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def ctextdefault(text):
-    """
-    Route for '/python' with an optional 'text' parameter
-    Replaces underscores in 'text' with spaces
-    """
-    return 'Python %s' % text.replace('_', ' ')
+@app.route('/c/<text>')
+def c_page(text):
+    '''
+    c page.
+    '''
 
-# Run the Flask application if this script is the main program
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>')
+@app.route('/python', defaults={'text': 'is cool'})
+def python_page(text):
+
+    '''
+    python page.
+    '''
+
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port='5000')
