2c59a8b83b91317af1d8e38b93a4ae6c2158e12f
simonkari <kariithisimon1715@gmail.com>
2023-09-24 18:40:09 +0300

    9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-"""
-Create a web app to display states as a list
-"""
-
+""""Create a web app to display states as a list"""
 from models import storage
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route('/states', strict_slashes=False)
 @app.route('/states/<state_id>', strict_slashes=False)
 def states(state_id=None):
-    """
-    Displays the states and cities in alphabetical order
-    """
+    """display the states and cities listed in alphabetical order"""
     states = storage.all("State")
     if state_id is not None:
         state_id = 'State.' + state_id
     return render_template('9-states.html', states=states, state_id=state_id)
 
+
 @app.teardown_appcontext
 def teardown(exc):
-    """
-    Closes the SQLAlchemy session
-    """
+    """Close sqlalchemy session"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
