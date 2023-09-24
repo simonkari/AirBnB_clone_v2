30c3beb0d659790ae4189dc49dd20ea88954952f
simonkari <kariithisimon1715@gmail.com>
2023-09-24 17:41:41 +0300

    state.py

--- models/state.py
+++ models/state.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
 """This is the state class"""
+
 from models.base_model import BaseModel, Base
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship, backref
 from os import environ
 import models
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship, backref
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
+    """Class for State with a name attribute"""
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    
     if 'HBNB_TYPE_STORAGE' in environ and environ['HBNB_TYPE_STORAGE'] == 'db':
-        cities = relationship('City', cascade="all, delete-orphan",
-                              backref="state")
+        cities = relationship('City', cascade="all, delete-orphan", backref="state")
     else:
         @property
         def cities(self):
-            lis = []
-            stat = self.id
-            for k, v in models.storage.all().items():
-                if "City" in k and v.state_id == self.id:
-                    lis.append(v)
-            return lis
+            """Returns a list of cities belonging to the state"""
+            city_list = []
+            state_id = self.id
+            for key, obj in models.storage.all().items():
+                if "City" in key and obj.state_id == state_id:
+                    city_list.append(obj)
+            return city_list
