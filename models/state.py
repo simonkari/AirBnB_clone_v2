03d40ef2d4568eb24e653fce09c7fe27c75d09fa
simonkari <kariithisimon1715@gmail.com>
2023-09-24 12:50:02 +0300

    state.py

--- models/state.py
+++ models/state.py
@@ -7,26 +7,32 @@ import models
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, Float
-storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
 class State(BaseModel, Base):
     """State class handles all application states"""
+    
     if storage_type == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state', cascade='delete')
+    
     else:
         name = ''
 
-        @property
-        def cities(self):
-            """
-                getter method, returns list of City objs from storage
-                linked to the current State
-            """
-            city_list = []
-            for city in models.storage.all("City").values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+class StateFileStorage(State):
+    """
+    State class with a file storage implementation for 'cities' property
+    """
+    @property
+    def cities(self):
+        """
+        Getter method, returns a list of City objects from storage
+        linked to the current State
+        """
+        city_list = []
+        for city in models.storage.all("City").values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
