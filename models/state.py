58cf998ab7876305ef221f445f0bf334535b4d10
simonkari <kariithisimon1715@gmail.com>
2023-09-24 11:10:17 +0300

    state.py

--- models/state.py
+++ models/state.py
@@ -6,6 +6,8 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import environ as env
 import models
+import os
+from models.city import City
 
 
 class State(BaseModel, Base):
@@ -16,18 +18,23 @@ class State(BaseModel, Base):
         name: input name
         cities: relation to cities table
     """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
-
-    if env.get('HBNB_TYPE_STORAGE') != 'db':
+    __tablename__ = 'states'
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship(
+            'City',
+            cascade='all, delete, delete-orphan',
+            backref='state'
+        )
+    else:
         @property
         def cities(self):
-            """get all cities with the current state id
-            from filestorage
-            """
-            list = [
-                value for key, value in models.storage.all(models.City).items()
-                if value.state_id == self.id
-            ]
-            return (list)
+            """Returns the cities in this State"""
+            from models import storage
+            cities_in_state = []
+            for value in storage.all(City).values():
+                if value.state_id == self.id:
+                    cities_in_state.append(value)
+            return cities_in_state
