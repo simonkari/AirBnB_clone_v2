0fd7b77d5b3ce11223dbb34b6e59e6d37698bae1
simonkari <kariithisimon1715@gmail.com>
2023-09-24 17:30:36 +0300

    5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,46 +1,78 @@
 #!/usr/bin/python3
 
 '''
-A simple Flask web app.
+An uncomplicated Flask web application.
 '''
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
 
-# Disable strict slashes for URL routing
+app = Flask(__name__)
+'''
+The instance of the Flask application.
+'''
+
 app.url_map.strict_slashes = False
+
 
 @app.route('/')
 def index():
-    '''Main page.'''
+    '''
+    Main page.
+    '''
+
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hbnb():
-    '''HBNB page.'''
+
+    '''
+    hbnb page.
+    '''
+
     return 'HBNB'
+
 
 @app.route('/c/<text>')
 def c_page(text):
-    '''C page.'''
+    '''
+    c page.
+    '''
+
     return 'C {}'.format(text.replace('_', ' '))
+
 
 @app.route('/python/<text>')
 @app.route('/python', defaults={'text': 'is cool'})
 def python_page(text):
-    '''Python page.'''
+    '''
+    python page.
+    '''
+
     return 'Python {}'.format(text.replace('_', ' '))
+
 
 @app.route('/number/<int:n>')
 def number_page(n):
-    '''Number page.'''
+    '''
+    The page related to numbers.
+    '''
+
     return '{} is a number'.format(n)
+
 
 @app.route('/number_template/<int:n>')
 def number_template(n):
-    '''Number template page.'''
-    return render_template('5-number.html', n=n)
+    '''
+    The page titled 'number_template'.
+    '''
+
+    ctxt = {
+        'n': n
+    }
+    return render_template('5-number.html', **ctxt)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
