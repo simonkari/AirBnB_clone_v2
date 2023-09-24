3b23588c52c8b4e9ffc52040736d23994251d7ca
simonkari <kariithisimon1715@gmail.com>
2023-09-24 18:06:28 +0300

    9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-""" List of states """
+"""
+Enumerates states
+"""
 
 
 from flask import Flask
+from flask import render_template
 from flask import session
 from models import storage
-from flask import render_template
 
 
 app = Flask(__name__)
@@ -13,12 +15,14 @@ app = Flask(__name__)
 
 @app.route('/states', strict_slashes=False)
 def states():
-    """lists states"""
+    """
+    Enumerates states
+    """
     storage.reload()
     cities = None
     states_dict = storage.all("State")
     states = []
-    for k, v in states_dict.items():
+    for s, k in states_dict.items():
         states.append([v.id, v.name])
     return render_template('9-states.html', states=states,
                            cities=cities, id=None)
@@ -26,20 +30,22 @@ def states():
 
 @app.route('/states/<id>', strict_slashes=False)
 def states_id(id):
-    """lists states"""
+    """
+    lists states
+    """
     storage.reload()
     cities_dict = storage.all("City")
     states_dict = storage.all("State")
     cities_states = []
     states = []
     state = []
-    for k, v in states_dict.items():
+    for s, k in states_dict.items():
         states.append([v.id, v.name])
     for city in cities_dict.values():
         if city.state_id == id:
             cities_states.append([city.id, city.name])
-    for s in states:
-        if s[0] == id:
+    for i in states:
+        if i[0] == id:
             state.append([s[0], s[1]])
     if (len(state) != 0):
         state = state[0][1]
@@ -51,7 +57,9 @@ def states_id(id):
 
 @app.teardown_appcontext
 def teardown_db(error):
-    """lists states"""
+    """
+    Enumerates states
+    """
     storage.close()
 
 if __name__ == '__main__':
