abea283782b169b6b6061b94b5f9e413d8f0c5e3
simonkari <kariithisimon1715@gmail.com>
2023-09-24 17:47:39 +0300

    db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
 """
-This module defines a database storage class for AirBnB clone.
 """
-
-from os import environ, getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from os import getenv
+from os import environ
+from models.base_model import BaseModel, Base
+from models.user import User
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-from models.base_model import BaseModel, Base
-from models.user import User
 from models.state import State
 from models.city import City
 
+
 class DBStorage():
     """
-    This class manages database storage for AirBnB clone.
     """
-
     __engine = None
     __session = None
-    supported_classes = [State, City, User, Place, Review, Amenity]
+    cla = [State, City, User, Place, Review, Amenity]
 
     def __init__(self):
-        """
-        Initializes a new DBStorage instance.
-        """
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
                                       format(getenv("HBNB_MYSQL_USER"),
                                              getenv("HBNB_MYSQL_PWD"),
@@ -38,56 +33,44 @@ class DBStorage():
 
     def all(self, cls=None):
         """
-        Returns a dictionary of objects in the database.
-
-        Args:
-            cls (str): The class name of objects to retrieve.
-
-        Returns:
-            dict: A dictionary of objects.
+        all
         """
-        objects = {}
+        dic = {}
         if cls is None:
-            for class_name in self.supported_classes:
-                rows = self.__session.query(class_name).all()
-                for row in rows:
-                    key = "{}.{}".format(class_name.__name__, row.id)
-                    objects[key] = row
-        else:
-            rows = self.__session.query(eval(cls)).all()
-            for row in rows:
-                key = "{}.{}".format(cls, row.id)
-                objects[key] = row
-        return objects
+            for c in self.cla:
+                row = self.__session.query(c).all()
+                for k in row:
+                    key = ".".join([c.__name__, k.id])
+                    dic.update({key: k})
+            return dic
+        row = self.__session.query(eval(cls)).all()
+        for k in row:
+            key = ".".join([cls, k.id])
+            dic.update({key: k})
+        return dic
 
     def new(self, obj):
         """
-        Adds a new object to the current database session.
-
-        Args:
-            obj (BaseModel): The object to add to the session.
+        new
         """
         self.__session.add(obj)
 
     def save(self):
         """
-        Commits all changes to the database session.
+        save
         """
         self.__session.commit()
 
     def delete(self, obj=None):
         """
-        Deletes an object from the current database session.
-
-        Args:
-            obj (BaseModel): The object to delete from the session.
+        delete
         """
-        if obj:
+        if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
         """
-        Reloads objects from the database.
+        reload
         """
         Base.metadata.create_all(self.__engine)
         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
@@ -95,6 +78,6 @@ class DBStorage():
 
     def close(self):
         """
-        Closes the current database session.
+        close
         """
         self.__session.close()
